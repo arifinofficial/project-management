31134ebeda6ed39026834a2236d979c0d22175ae
Arifin N <arifinofficial30@gmail.com>
2020-06-25 15:02:07 +0800

    Dynamic content job

--- resources/views/job/index.blade.php
+++ resources/views/job/index.blade.php
@@ -29,7 +29,7 @@
 <!-- Page Content -->
 <div class="container-fluid mt--6">
     <div class="row justify-content-center">
-        @foreach ($jobs as $job)
+        @foreach ($jobs as $key => $job)
             <div class="col-4">
                 <div class="card">
                     <div class="card-header bg-transparent">
@@ -38,16 +38,18 @@
                     <div class="card-body text-center">
                         <div class="content">
                             <div class="wrapper-badge mb-4">
-                                <span class="badge badge-primary">10 Divisi</span>
-                                <span class="badge badge-success">10 Pekerjaan</span>
+                                <span class="badge badge-primary">{{ count($job->departements) }} Departement</span>
+                                <span class="badge badge-success">
+                                    {{ $job->taskCount() }} Pekerjaan
+                                </span>
                             </div>
                             <p class="small"><i class="ni ni-time-alarm d-block mb-1"></i> {{ $job->start }} - {{ $job->end }}</p>  
                             <div class="d-flex align-items-center">
-                                <span class="completion mr-2 small">60%</span>
+                                <span class="completion mr-2 small">{{ $job->progress }}%</span>
                                 <div class="w-100">
                                     <div class="progress m-0" style="width: 100%; height: 3px;">
-                                        <div class="progress-bar bg-warning" role="progressbar" aria-valuenow="60" aria-valuemin="0"
-                                            aria-valuemax="100" style="width: 60%;"></div>
+                                        <div class="progress-bar {{ $job->progress < 50 ? 'bg-warning' : ($job->progress < 80 ? 'bg-primary' : 'bg-success') }}" role="progressbar" aria-valuenow="60" aria-valuemin="0"
+                                            aria-valuemax="100" style="width: {{ $job->progress }}%;"></div>
                                     </div>
                                 </div>
                             </div>
