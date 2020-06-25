7ae1a4188f83e1afe0f590349822d0bfa1156ea2
Arifin N <arifinofficial30@gmail.com>
2020-06-25 15:01:32 +0800

    new footer

---
+++ resources/views/layouts/partials/footer.blade.php
@@ -0,0 +1,24 @@
+<div class="container-fluid">
+    <hr class="mb-0 mt-5">
+    <footer class="footer">
+        <div class="row align-items-center justify-content-lg-between">
+            <div class="col-lg-6">
+                <div class="copyright text-center  text-lg-left  text-muted">
+                    © 2020 <a href="#" class="font-weight-bold ml-1"
+                        target="_blank">Arifin N</a>
+                </div>
+            </div>
+            <div class="col-lg-6">
+                <ul class="nav nav-footer justify-content-center justify-content-lg-end">
+                    <li class="nav-item">
+                        <a href="https://github.com/arifinofficial/project-management" class="nav-link" target="_blank">Source Code</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="https://github.com/arifinofficial/project-management/blob/master/LICENSE"
+                            class="nav-link" target="_blank">MIT License</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </footer>
+</div>
