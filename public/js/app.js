95af80e8d668ba19ff7cd9bf3e85333b5ba56a90
Arifin N <arifinofficial30@gmail.com>
2020-07-20 01:49:46 +0800

    Webpack

--- public/js/app.js
+++ public/js/app.js
@@ -69353,15 +69353,27 @@ __webpack_require__.r(__webpack_exports__);
 
 /***/ }),
 
+/***/ "./resources/sass/report.scss":
+/*!************************************!*\
+  !*** ./resources/sass/report.scss ***!
+  \************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+// removed by extract-text-webpack-plugin
+
+/***/ }),
+
 /***/ 0:
-/*!*************************************************************!*\
-  !*** multi ./resources/js/app.js ./resources/sass/app.scss ***!
-  \*************************************************************/
+/*!******************************************************************************************!*\
+  !*** multi ./resources/js/app.js ./resources/sass/app.scss ./resources/sass/report.scss ***!
+  \******************************************************************************************/
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
 __webpack_require__(/*! /Volumes/Developments/Website/project-management/resources/js/app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! /Volumes/Developments/Website/project-management/resources/sass/app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! /Volumes/Developments/Website/project-management/resources/sass/app.scss */"./resources/sass/app.scss");
+module.exports = __webpack_require__(/*! /Volumes/Developments/Website/project-management/resources/sass/report.scss */"./resources/sass/report.scss");
 
 
 /***/ })
